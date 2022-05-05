1de8089f77dc9bdad4b15a794e037974f17d0d45
siddhuWooqer <siddhant.jaiswal@wooqer.com>
2022-05-05 16:25:53 +0530

    Initial Commit

---
+++ p2module/src/main/java/com/wooqer/p2module/ToasterMessage.java
@@ -0,0 +1,12 @@
+package com.wooqer.p2module;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToasterMessage {
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+
+    }
+}
